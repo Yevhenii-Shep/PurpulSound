c681e06d995fe5c316a63e440b4acabcf975774c
Yevhenii-Shep <yevhenii.shepetia@gmail.com>
2026-01-21 15:16:45 +0100

    final

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,6 @@
 <script>
-import { useCartStore } from '@/stores/cart'
 export default {
-  name: 'AppHeader',
-
-  computed: {
-    cartCount() {
-      const cart = useCartStore()
-      return cart.totalCount
-    }
-  }
-
+  name: 'AppHeader'
 }
 </script>
 
@@ -17,13 +8,10 @@ export default {
   <header>
     <div class="header-inner">
       <h1 class="logo">PurpleSound</h1>
-
       <nav class="menu">
         <router-link to="/">Home</router-link>
         <router-link to="/products">Products</router-link>
-        <router-link to="/cart">Cart<span v-if="cartCount > 0" class="cart-badge">
-      {{ cartCount }}
-    </span></router-link>
+        <router-link to="/cart">Cart</router-link>
       </nav>
     </div>
   </header>
@@ -36,8 +24,7 @@ header {
 
 .header-inner {
   max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem clamp(1rem, 4vw, 2rem);
+  padding: 1rem 2rem;
 
   display: flex;
   justify-content: space-between;
@@ -46,20 +33,13 @@ header {
 
 .logo {
   color: #c77dff;
-  font-size: clamp(1.4rem, 2.5vw, 1.8rem);
-  white-space: nowrap;
-}
-
-.menu {
-  display: flex;
-  gap: clamp(1rem, 3vw, 2rem);
+  margin-right: 280%;
 }
 
 .menu a {
+  margin-left: 1.5rem;
   color: #b57cff;
   font-weight: 500;
-  text-decoration: none;
-  white-space: nowrap;
 }
 
 .menu a:hover {
